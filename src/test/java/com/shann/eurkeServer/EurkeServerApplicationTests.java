0276eb2736a5d00cd838d1b6bae28d0682eda454
shantanukumar <shan.raj93@gmail.com>
2024-02-12 13:06:15 +0530

    technical(eurekaServer): init commit

---
+++ src/test/java/com/shann/eurkeServer/EurkeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shann.eurkeServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurkeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
